33b23f44c347227f47dacca912bcf8b279a0c7f5
Bram Snutters <bram2268@gmail.com>
2019-09-29 13:56:59 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -42,7 +42,7 @@ class Proxy
      * URI to add before relative urls as well as default URL
      * @var string
      */
-    protected $prefix = 'http://ahoy.one';
+    protected $prefix = 'https://thepiratebay.org';
     
     /**
      * Url to proxy.php
@@ -246,29 +246,3 @@ try {
 } catch(Exception $e) {
     echo 'Error: '.$e->getMessage();
 }
-
-<!-- PopAds.net Popunder Code for www.tpbay.ga -->
-<script type="text/javascript" data-cfasync="false">
-/*<![CDATA[/* */
-  var _pop = _pop || [];
-  _pop.push(['siteId', 1381494]);
-  _pop.push(['minBid', 0]);
-  _pop.push(['popundersPerIP', 0]);
-  _pop.push(['delayBetween', 0]);
-  _pop.push(['default', false]);
-  _pop.push(['defaultPerDay', 0]);
-  _pop.push(['topmostLayer', false]);
-  (function() {
-    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
-    var s = document.getElementsByTagName('script')[0]; 
-    pa.src = '//c1.popads.net/pop.js';
-    pa.onerror = function() {
-      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
-      sa.src = '//c2.popads.net/pop.js';
-      s.parentNode.insertBefore(sa, s);
-    };
-    s.parentNode.insertBefore(pa, s);
-  })();
-/*]]>/* */
-</script>
-<!-- PopAds.net Popunder Code End -->
